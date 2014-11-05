effee3df5e621d53cee8d184dbd88b0904ba73ea
aquaxp <aquaxp@users.noreply.github.com>
2014-11-05 12:33:55 +0600

    cleaning bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,28 +6,11 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "Brewfile" --exclude "nophonehome.py" \
+		--exclude "README.md" --exclude "./brew.sh" --exclude "nophonehome.py" \
 		--exclude ".osx" --exclude ".linux" \
-		--exclude "Caskfile" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+		--exclude "./cask.sh" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 
 	source ~/.bash_profile;
-
-	read -p "Do some OS specific things like \"brewing\" \"casking\" and setting some defaults (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		if [ `uname` == "Darwin" ]; then
-			python nophonehome.py;
-			./.osx;
-			ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
-
-			brew bundle Brewfile;
-			brew bundle Caskfile;
-		fi;
-
-		if [ `uname` == "Linux" ]; then
-			./.linux;
-		fi;
-	fi;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
