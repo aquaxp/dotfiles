ff1ec14e2e97e1a6c8fe024c65a6f314f3454383
Orte Ga <aquaxpi@gmail.com>
2017-04-01 12:05:42 -0700

    .brew.sh: Automate switch to brew-installed Bash

--- brew.sh
+++ brew.sh
@@ -25,6 +25,12 @@ function brewInst() {
 	brew tap homebrew/versions
 	brew install bash-completion2
 
+	# Switch to using brew-installed bash as default shell
+	if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+		echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+		chsh -s /usr/local/bin/bash;
+	fi;
+
 	# Install wget with IRI support
 	brew install wget --with-iri
 
