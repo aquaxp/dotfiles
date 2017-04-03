a58993be1838c1c609d406d5eb228e5d3a107c21
Orte Ga <aquaxpi@gmail.com>
2017-04-02 18:33:14 -0700

    brew.sh: Update vim installation command

--- brew.sh
+++ brew.sh
@@ -35,7 +35,7 @@ function brewInst() {
 	brew install wget --with-iri
 
 	# Install more recent versions of some OS X tools
-	brew install vim --override-system-vi
+	brew install vim --with-override-system-vi
 	brew install homebrew/dupes/grep
 	brew install homebrew/dupes/screen
 	#install homebrew/php/php55 --with-gmp
