403537586bc16bdcd0f687340089a44e54056515
Orte Ga <aquaxpi@gmail.com>
2018-03-12 22:48:21 -0700

    brew.sh: Remove dependency on homebrew/versions

--- brew.sh
+++ brew.sh
@@ -21,7 +21,6 @@ function brewInst() {
 	# Install Bash 4
 	# Note: don't forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
 	brew install bash
-	brew tap homebrew/versions
 	brew install bash-completion2
 
 	# Switch to using brew-installed bash as default shell
